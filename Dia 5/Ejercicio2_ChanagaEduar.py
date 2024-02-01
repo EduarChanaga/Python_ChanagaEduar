e053be5abed211edb459841d69ceaf79ac2e26cf
Eduar <chanaga250@gmail.com>
2024-02-01 07:23:13 -0500

    avance

--- Dia 5/Ejercicio2_ChanagaEduar.py
+++ Dia 5/Ejercicio2_ChanagaEduar.py
@@ -1,11 +1,18 @@
+from functools import reduce
+from math import gcd
 # m = monedas
 # me = mesas
 # mo = lista para almacenar las diferentes longitudes de monedas
 # me2 = lista para almacenar la altura de las patas de las mesas deseadas
+def lcm(a, b):
+    return abs(a * b) // gcd(a, b) if a and b else 0
 
-def maximo(maxcoins):
-    print("hi")
-    print(maxcoins)
+def find_lcm_of_list(list):
+    return reduce(lcm, list)
+
+# Ejemplo de uso:
+
+
 
 m, me = map(int, input("Enter m and me: ").split())
 mo = []
@@ -21,16 +28,6 @@ for i in range(me):
 # Mostrar las listas
 print("monedas:", mo)
 print("mesas:", me2)
-maxcoins = max(mo)
 
-while True:
-    element=me2[count]
-    if log>=element:
-        break
-    else:
-        log=maxcoins+log
-        print(log)
-
-    count=count+1
-    
-#programacion de mrd .i.
+resultado = find_lcm_of_list(mo)
+print(f"El mínimo común múltiplo de {mo} es: {resultado}")
