47c42a769cb5d3b25ac88bd74efad821c0f1858f
eduar <chanaga250@gmail.com>
2024-02-02 07:03:26 -0500

    comentarios

--- Dia 6/Diccionario_ChanagaEduar.py
+++ Dia 6/Diccionario_ChanagaEduar.py
@@ -1,20 +1,23 @@
-panaderia = {"Inventario": ("rosquilla", "Cascarita", "churros", "Pan de queso"),
-            "Precios": (3000, 1000, 2500, 2000)}
+# Almacena los productos con su respectivo precio en un diccionario.
+panaderia = {"inventario": ("rosquilla", "cascarita", "churros", "pan de queso"),
+            "Precios": (3000, 1000, 2500, 2000)
+            }
 
 # Muestra el menú
 print("Menu actual:")
 print(panaderia)
 
+# Pide datos de compra y cantidad
 p = str(input("Ingrese lo que desea comprar de la lista anterior: "))
 c = int(input("Ingrese la cantidad que desea de este producto: "))
 
-
-if p in panaderia["Inventario"]:
-                        #.index se usa para obtener la posición del producto p en la lista de inventario.
-    index = panaderia["Inventario"].index(p)
-   
+# Comprueba si el caracter ingresado se encuentra en el diccionario de inventario
+if p in panaderia["inventario"]:
+    # .index se usa para obtener la posición del producto p en la lista de inventario.
+    # y lo almacena en la variable index
+    index = panaderia["inventario"].index(p)
+    # Según el index anterior, halla la ubicación del precio de este producto.
     precio = panaderia["Precios"][index]
-   
-    print(f"{p} - Precio: {precio*c} - Cantidad: {c}")
+    print(f"Producto: {p}\n Precio: {precio*c}\n Cantidad: {c}")
 else:
     print(f"{p} no está en el inventario.")
